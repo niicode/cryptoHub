51fb8d6f4b7f4ca00e3299111ea9fbf03bf166f6
niicode <niidarku66@gmail.com>
2022-10-04 13:54:15 +0000

    Add components and redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+/* eslint-disable import/extensions */
+import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { fetchCurrencyList } from './redux/currencySlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const currencyState = useSelector((state) => state.currency);
+
+  useEffect(() => {
+    if (currencyState.status === 'idle') {
+      dispatch(fetchCurrencyList());
+    }
+  }, [
+    dispatch,
+    currencyState.status,
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Outlet />
+    </>
   );
 }
 
